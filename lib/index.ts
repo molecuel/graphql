709cd72ffbbe06891ddd4c3db13665968efb90f1
Daniel Walther <daniel.walther@inspirationlabs.com>
2018-01-29 15:51:19 +0100

    unit tests with typedef

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,101 @@
 "use strict";
 import "reflect-metadata";
 
+import {MlclCore} from "@molecuel/core";
 import {di, injectable, singleton} from "@molecuel/di";
+import { MlclElements } from "@molecuel/elements";
 import {Observable, Subject} from "@reactivex/rxjs";
 import {
-  graphql,
+  GraphQLBoolean,
+  GraphQLFloat,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
   GraphQLObjectType,
   GraphQLSchema,
-  GraphQLString} from "graphql";
+  GraphQLString,
+  printSchema,
+} from "graphql";
+
+@injectable
+export class MlclGraphQL {
+  private elems: MlclElements = di.getInstance("MlclElements");
+  private gqlStore: any = new Map();
+
+  constructor() {
+    di.bootstrap(MlclCore, MlclElements);
+  }
+
+  /**
+   * Returns a graphql schema for all registered Elements
+   *
+   * @memberOf MlclGraphQL
+   */
+  public renderGraphQL() {
+    const elemProperties = this.elems.getMetadataTypesForElements();
+    const keys = Object.keys(elemProperties);
+    for (const key of keys) {
+      const item = this.renderGqlItem(key, elemProperties);
+      this.gqlStore.set(key, item);
+    }
+    const queryType = {
+      description: "The root query type.",
+      fields: {},
+      name: "Query",
+    };
+
+    for (const [key, gqlElement] of this.gqlStore) {
+      queryType.fields[key] = {
+        type: gqlElement,
+      };
+    }
+
+    const queryGqlType = new GraphQLObjectType(queryType);
+
+    const schema = new GraphQLSchema({
+      query: queryGqlType,
+    });
+
+    return printSchema(schema);
+  }
+
+  /**
+   * Renders a Object type as graphql object type
+   *
+   * @param  {string} name              [The item name]
+   * @param {object} definitions        [The property definitions of the elements]
+   *
+   * @memberOf MlclGraphQL
+   */
+  public renderGqlItem(name: string, definitions: any) {
+    const gqlObjDef = {
+      fields: {},
+      name,
+    };
+    for ( const prop of definitions[name]) {
+      let gqlType;
+      if (prop.type === "Number") {
+        gqlObjDef.fields[prop.property] = {};
+        gqlType = GraphQLFloat;
+      } else if (prop.type === "String") {
+        gqlObjDef.fields[prop.property] = {};
+        gqlType = GraphQLString;
+      } else if (prop.type === "Boolean") {
+        gqlObjDef.fields[prop.property] = {};
+        gqlType = GraphQLBoolean;
+      } else {
+        gqlObjDef.fields[prop.property] = {};
+        if (this.gqlStore.get(prop.type)) {
+          gqlType = this.gqlStore.get(prop.type);
+        } else {
+          gqlType = this.renderGqlItem(prop.type, definitions);
+        }
+      }
+      if (prop.nested) {
+        gqlType = new GraphQLList(gqlType);
+      }
+      gqlObjDef.fields[prop.property].type = gqlType;
+    }
+    return new GraphQLObjectType(gqlObjDef);
+  }
+}
--- test/index.ts
+++ test/index.ts
@@ -1,13 +1,38 @@
+"use strict";
+import * as fs from "fs";
 import "reflect-metadata";
+import * as supertest from "supertest";
 
 import * as chai from "chai";
-const should = chai.should;
+const should = chai.should();
 const expect = chai.expect;
 const assert = chai.assert;
 
-import {MlclCore} from "@molecuel/core";
-import {di, injectable} from "@molecuel/di";
+import { MlclCore } from "@molecuel/core";
+import { di, injectable } from "@molecuel/di";
+import { MlclGraphQL } from "../lib";
 
-describe("<%= name %>", function() {
+import { Robot } from "./data";
 
+// tslint:disable:no-console
+describe("graphql", () => {
+  let coreInitSuccess: boolean = false;
+  describe("schemas", () => {
+    const mlclGql: MlclGraphQL = di.getInstance("MlclGraphQL");
+    it("init core", async () => {
+      di.bootstrap(
+        MlclGraphQL,
+        Robot,
+      );
+      const core: MlclCore = di.getInstance("MlclCore");
+      await core.init();
+      should.exist(core);
+      core.should.be.instanceOf(MlclCore);
+      coreInitSuccess = true;
+    });
+    it("should get the type definition for all registered elements", () => {
+      const typedefs = mlclGql.renderGraphQL();
+      expect(typedefs).to.be.a("string");
+    });
+  }); // category end
 }); // test end
